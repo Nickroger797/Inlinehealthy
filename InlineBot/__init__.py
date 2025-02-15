8f965056833b35217ccd7cf1e0369f5218d4b4ce
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-02-16 03:08:00 +0530

    Update __init__.py

--- InlineBot/__init__.py
+++ InlineBot/__init__.py
@@ -76,6 +76,7 @@ class CodeXBotz(Client):
        await app.setup()
        bind_address = "0.0.0.0"
        await web.TCPSite(app, bind_address, PORT).start()
+       await idle()
 
     async def stop(self, *args):
         await super().stop()
