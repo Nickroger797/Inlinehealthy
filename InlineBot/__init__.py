0a30a86246831e154addae2750a8dc5b88f6a6a9
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-03-28 10:10:18 +0530

    Update __init__.py

--- InlineBot/__init__.py
+++ InlineBot/__init__.py
@@ -7,7 +7,7 @@ import time
 import requests
 import shutil
 from InlineBot.server import start_web
-from InlineBot.time_sync import sync_time, TIME_OFFSET
+from InlineBot.time_sync import sync_time
 from InlineBot.time_sync import *
 
 API_HASH = os.environ.get("API_HASH", "05be4bb2e1e6806a2ffd23402079e23a")
