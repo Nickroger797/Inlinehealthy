464bb3990a0d7f0bf56d143f6877a92721f3685f
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-03-28 13:11:58 +0530

    Update base_commands.py

--- InlineBot/plugins/base_commands.py
+++ InlineBot/plugins/base_commands.py
@@ -7,7 +7,7 @@ from InlineBot import CodeXBotz, ADMINS, filters, Message, InlineKeyboardButton,
 from InlineBot.strings import START_MESSAGE, HELP_MESSAGE, ABOUT_MESSAGE, MARKDOWN_HELP
 from InlineBot.database import present_in_userbase, add_to_userbase
 from pyrogram.enums import ParseMode
-from InlineBot.__init__.py import IS_PUBLIC
+from InlineBot import IS_PUBLIC
 
 start_keyboard = [
     [
